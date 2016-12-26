7932711dbf024c680800b1d23261d42627dd7e76
stoad <yag0308@naver.com>
2016-12-26 15:13:38 +0900

    Create calculater.h

---
+++ calculater.h
@@ -0,0 +1,3 @@
+double calculater(double arcs,char *str);       //calculating funtion(calculating)
+int buildTree(char *str);                       //buildTree(graph)
+double solveEquation(double x,double y);        //graph
